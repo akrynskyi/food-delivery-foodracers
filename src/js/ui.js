477700bdf1f884911e6f2629440ed26cca80d1a5
Andrii Krinskiy <andrewkrinskiy@gmail.com>
2020-01-21 02:37:27 +0200

    add gallery

--- src/js/ui.js
+++ src/js/ui.js
@@ -12,3 +12,25 @@ closeNav.addEventListener("click", () => {
   sidebarNav.classList.remove("sidebar__open");
   overlay.classList.remove("overlay__visible");
 });
+
+// gallery
+const images = document.querySelectorAll(".gallery-image");
+const lightbox = document.querySelector(".lightbox");
+const imageContainer = document.querySelector(".lightbox-container");
+
+images.forEach(image => {
+  image.addEventListener("click", () => {
+    lightbox.classList.add("active");
+    const lightboxImage = document.createElement("img");
+    lightboxImage.classList.add("lightbox-image");
+    lightboxImage.src = image.src;
+    while (imageContainer.firstChild) {
+      imageContainer.removeChild(imageContainer.firstChild);
+    }
+    imageContainer.appendChild(lightboxImage);
+  });
+});
+lightbox.addEventListener("click", e => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+});
